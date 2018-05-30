f74c6243ea0c3fba13ba33c2822d7440f2ba309f
Carlos Jiménez <cojimene@gmail.com>
2018-05-29 20:22:10 -0500

    makes a todo list for iOS

--- TodoList/ViewController.swift
+++ TodoList/ViewController.swift
@@ -8,8 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var taskField: UITextField!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var taskList: UITableView!
+    
+    var todoList = ["Study swift", "Practice Xcode"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return(todoList.count)
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = todoList[indexPath.row]
+        return(cell)
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            todoList.remove(at: indexPath.row)
+            taskList.reloadData()
+        }
+    }
+    
+    @IBAction func addTask(_ sender: Any) {
+        if taskField.text != "" {
+            todoList.append(taskField.text!)
+            taskList.reloadData()
+            taskField.text = ""
+        }
+    }
+    
 }
 
